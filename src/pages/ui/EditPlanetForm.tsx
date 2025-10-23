feded05de3904062ec65bf372fb9dc454ecd8777
Ale Roses <ale.vrs@outlook.com>
2025-10-23 17:32:21 -0500

    30.8 useFormStatus - Parent form status

--- src/pages/ui/EditPlanetForm.tsx
+++ src/pages/ui/EditPlanetForm.tsx
@@ -1,6 +1,7 @@
-import { useActionState, useState } from "react";
+import { useActionState } from "react";
 import { Planet } from "../../interfaces/planet.interface";
 import { createPlanetActionForm } from "../../actions/create-planet.action";
+import { SubmitButton } from "./SubmitButton";
 
 interface Props {
   onAddPlanet: (planet: Planet) => void;
@@ -18,22 +19,13 @@ export const EditPlanetForm = ({ onAddPlanet }: Props) => {
     null
   );
 
-  // const [name, setName] = useState('');
-  // const [type, setType] = useState('');
-  // const [distanceFromSun, setDistanceFromSun] = useState('');
-
-  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   onAddPlanet({ name, type, distanceFromSun });
-  // };
-
   return (
     <form
       className="mb-4 flex flex-col md:flex-row"
       // onSubmit={handleSubmit}
       action={formAction}
     >
-      <h1>{isPending ? "Pending" : "No pending"}</h1>
+      {/* <h1>{isPending ? "Pending" : "No pending"}</h1> */}
       <input
         type="text"
         placeholder="Nombre del planeta"
@@ -55,13 +47,15 @@ export const EditPlanetForm = ({ onAddPlanet }: Props) => {
         name="distanceFromSun"
         required
       />
-      <button
+
+      <SubmitButton />
+      {/*   <button
         type="submit"
         className="bg-blue-500 disabled:bg-gray-500 text-white p-2 rounded flex-1 sm:flex-none"
         disabled={isPending}
       >
         Agregar planeta
-      </button>
+      </button> */}
     </form>
   );
 };
